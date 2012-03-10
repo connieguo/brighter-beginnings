e5b4ecde82d3501a64ba49cdd8b3cec463723b01
Connie Guo <connie.guo107@gmail.com>
2012-03-10 00:55:18 -0800

    rails app initiated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrighterBeginnings::Application.config.secret_token = '4ddc3aee4549e8bbe128a0ba94179dd65a42f1a72c61983fe1098a10eab9211c94985a1f2cfc177c372ad7e619281f1e552d0944fd62c0dd54c0f2720f697786'
