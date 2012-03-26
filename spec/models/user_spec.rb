0199507299ed4f61229693adb6549fba597bf1ea
saasbook <saasbook@saasbook.(none)>
2012-03-25 17:53:18 -0700

    delete case manager feature file?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "delete user" do
+  
+    it "returns false if the logged in account does not have valid permissions to delete target account" do
+    end
+  
+    it "returns true if the logged in account successfully deletes target account" do
+    end  
+  
+    it "deletes the appropriate accounts" do
+    end
+  end
+  
 end
