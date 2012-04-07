ff3cd98a7c01f76d3de11e5c2db1df1c2965c518
Connie Guo <connie.guo107@gmail.com>
2012-04-07 13:56:25 -0700

    added has_many associations for user and family

--- app/models/family.rb
+++ app/models/family.rb
@@ -3,6 +3,7 @@ class Family < ActiveRecord::Base
   validates_presence_of :location
 
   has_many :family_members
+  has_many :users, :through => :donations
   def self.get_sizes
     return ["2", "3", "4", "5", "6 or more"]
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_presence_of :identity, :locationID
-
+  has_many :families, :through => :donations
   has_many :donations
 end
