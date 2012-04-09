a9135d7cc613073b10a0b9dd844de303faec5a30
Connie Guo <connie.guo107@gmail.com>
2012-04-08 23:43:26 -0700

    added model method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,12 @@ require 'spec_helper'
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
   
-  describe "delete user" do
-  
-    it "returns false if the logged in account does not have valid permissions to delete target account" do
-    end
-  
-    it "returns true if the logged in account successfully deletes target account" do
-    end  
-  
-    it "deletes the appropriate accounts" do
-    end
+  describe 'get nearby families' do
+     it 'should get all the nearby families' do
+       @fake_families = [mock('Family'), mock('Family')]
+       Family.should_receive(:find_all_by_locationID).with('1').and_return(@fake_families)
+       User.findNearbyFamilies('1')
+     end
   end
   
 end
