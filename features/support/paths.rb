c98fa16b1cc6a34be874f92538d4da66a33b93a5
Connie Guo <connie.guo107@gmail.com>
2012-04-08 22:48:27 -0700

    modified paths

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,8 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+    when /^the donors home page$/
+      '/users/main'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
