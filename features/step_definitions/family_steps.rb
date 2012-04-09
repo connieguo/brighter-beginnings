bb2a6b98b2878c7a209f63aef3aa2c1efcf86a0e
Connie Guo <connie.guo107@gmail.com>
2012-04-08 23:09:23 -0700

    added steps def

--- features/step_definitions/family_steps.rb
+++ features/step_definitions/family_steps.rb
@@ -6,5 +6,16 @@ Given /^the following families exist:$/ do |families_table|
   end
 end
 
+Given /^the following family members exist:$/ do |family_members_table|
+  family_members_table.hashes.each do |family_member|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    FamilyMember.create(family_member)
+  end
+end
+
+
+
+
 
 
