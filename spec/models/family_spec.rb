38586ecd63eaa6199ef50240220fb67bf5049206
Connie Guo <connie.guo107@gmail.com>
2012-04-15 15:27:37 -0700

    family spec model methods

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -4,9 +4,15 @@ describe Family do
   pending "add some examples to (or delete) #{__FILE__}"
   describe 'get_size' do
      it 'should get the size of a particular family' do
-       @fake_family_members = [mock('Family'), mock('Family')]
        FamilyMember.should_receive(:find_all_by_family_code).with('OAB123').should_receive(:count).and_return(1)
        Family.get_size('OAB123')
+     end
+  end
+  describe 'get_pending' do
+     it 'should get all families whose blurbs have not been approved yet' do
+       @fake_pending_families = [mock('Family'), mock('Family')]
+       Family.should_receive(:find_all_by_approved).with("true").and_return(@fake_pending_families)
+       Family.get_pending()
      end
   end
 end
