0eab7acb2f7b4ad2e8e373f7e7086293d9a3c1b3
Connie Guo <connie.guo107@gmail.com>
2012-04-20 21:54:51 -0700

    pushing everything to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ BrighterBeginnings::Application.routes.draw do
   match '/auth/google/callback', :to => 'sessions#create', :as => 'login_callback'
   match '/auth/failure', :to => 'sessions#failed_login'
   match '/logout', :to => 'sessions#logout'
+  match '/families/:id/family_members', :to => 'family_members#index', :as => 'family_details'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
