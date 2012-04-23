0971041f838c7ac715141bb068bab1159bcf3253
Connie Guo <connie.guo107@gmail.com>
2012-04-22 21:43:23 -0700

    finished donation sp

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Donation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @donation = Donation.new(:id=>1, :user_id=>1, :dropoff_site=>1)
+  end
+  describe 'get_donor' do
+    it "should find the donor for this donation" do
+      @fake_user = mock('User')
+      User.should_receive(:find_by_id).with(@donation.user_id).and_return(@fake_user)
+      @donation.get_donor.should == @fake_user
+    end
+  end  
+
+  describe 'get_location' do
+    it "should get the location of the family" do
+      Family.should_receive(:get_location_name).with(@donation.dropoff_site).and_return("Oakland")
+      @donation.get_location
+    end
+  end
 end
+
