0a155ffb60e41c73b237d07794d7dc8bd4ca6a6c
Mark Peng <mahkuh@berkeley.edu>
2012-05-03 07:58:19 -0700

    email template model spec done

--- spec/models/email_template_spec.rb
+++ spec/models/email_template_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe EmailTemplate do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "get current template" do
+    it "should return the newest email template" do
+      @template = mock('EmailTemplate')
+      EmailTemplate.should_receive(:find).and_return(@template)
+      EmailTemplate.get_current_template.should == @template
+    end
+  end
 end
